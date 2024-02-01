261d7a3521e458b777777e19ba296994ace86441
soulayma ben amara <s.benamara22359@pi.tn>
2024-02-01 16:38:37 +0100

    code de seance 1

---
+++ seance1/exemple1.py
@@ -0,0 +1,8 @@
+# en mode script,on ecrit les codes dans un fichier .py
+nom="Ali" #variable de type str
+age=30 #variable de type int
+salaire="1200.7563" #variable de type float
+est_celibataire=True #variable de type booléen
+#affichage les informations de la personne 
+#on appelle les fonctions print
+print(nom)
